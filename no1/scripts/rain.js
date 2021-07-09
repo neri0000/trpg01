043d19750bd30d2d9bf6ffad930c04e373eede11
Shuhei Koizumi <mozi3021@gmail.com>
2021-07-09 10:38:15 +0900

    ボタン追加

--- no1/scripts/rain.js
+++ no1/scripts/rain.js
@@ -18,13 +18,54 @@ function Flag() {
   }
 }
 
+const events = window.document;
+
 //ウィンドウをクリックでフラグを対処
-document.body.onclick = Room1()
+events.body.onclick = Room1()
 
 function Room1() {
   const some = document.querySelector('p');
   some.textContent = "文字を変更"
+
 }
+
+ItemClick()
+function ItemClick(){
+  // 追加ボタンをinputタグで作り、bodyタグの子要素に入れる
+  let key = 0;
+  const addButton = document.createElement('input');
+  addButton.classList.add('addition');
+  addButton.type = 'button';
+  addButton.value = '追加';
+  document.body.appendChild(addButton);
+
+  // 追加ボタンをクリックしたら、ナンバー付の削除ボタンをinputタグで作る関数
+  function buttonAdd() {
+      const addButtonClick = document.getElementsByClassName('addition')[0];
+      addButtonClick.addEventListener('click', ()=> {
+          const delButton = document.createElement('input');
+          delButton.classList.add('deletion');
+          delButton.type = 'button';
+          delButton.value = `${key}：削除`;
+          document.body.appendChild(delButton);
+          key++;
+          buttonDelete();
+      }, false);
+  }
+
+  //クリックした削除ボタンを取る関数（実は非表示にするだけ）
+  function buttonDelete() {
+      for (let i = 0; i < key; i++) {
+          const deleteButtonClick = document.getElementsByClassName('deletion')[i];
+          deleteButtonClick.addEventListener('click', ()=> {
+              deleteButtonClick.classList.add('hidden');
+          }, false);
+      }
+  }
+
+  buttonAdd()
+}
+
 first()
 function first() {
   Flag();
