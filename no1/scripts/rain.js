c2b12cf889a739cf4d678297006761d8e9460528
neri0000 <hanna.827.kuro.96@gmail.com>
2021-07-16 06:41:35 +0900

    導入

--- no1/scripts/rain.js
+++ no1/scripts/rain.js
@@ -1,99 +1,72 @@
-
 const canvas = document.getElementById('canvas');
 const c = canvas.getContext('2d');
 
 const width = canvas.width = window.innerWidth;
 const height = canvas.height = window.innerHeight;
 
-//フラグ管理
-function Flag() {
-  let flagjudge = [];
-  let trueEnd = false;
+//ランダム設定
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
 
-  for(let i=0; i<flagjudge.length; i++){
-    flagjudge[i] = false;
+//成形
+function Shape(x, y, velY) {
+  this.x = x;
+  this.y = y;
+  this.velY = velY;
+}
+
+//雨の設定
+function Rain(x, y, step,size) {
+  Shape.call(this,x,y,step);
+  this.size = size;
+}
+
+Rain.prototype = Object.create(Shape.prototype);
+Rain.prototype.constructor = Rain;
+
+
+//描画処理
+Rain.prototype.draw = function() {
+  c.beginPath();
+  c.fillStyle = 'rgb(0, 0, 0,0.3)';
+  c.lineWidth = 0.5;
+  c.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+  c.fill();
+};
+
+//更新
+Rain.prototype.update = function() {
+  if((this.y + this.size) >= height) {
+    this.y = 0;
+    this.x = random(this.size,width-this.size);
   }
+  this.y += this.velY;
+};
 
-  if (flagjudge[0]) {
-    trueEnd = true;
+
+
+const rains = [];
+
+while(rains.length < 10) {
+  const size = random(2,5);
+  let rain = new Rain(
+    random(size,width - size),
+    random(size,height - size),
+    size*0.3,size
+  );
+  rains.push(rain);
+}
+
+function loop() {
+  c.fillStyle = 'rgba(193, 189, 189)';
+  c.fillRect(0,0,width,height);
+
+  for(let i = 0; i < rains.length; i++) {
+      rains[i].draw();
+      rains[i].update();
   }
+  requestAnimationFrame(loop);
 }
-
-//ウィンドウをクリックでフラグを対処
-document.body.onclick = Room1()
-
-function Room1() {
-  const some = document.querySelector('p');
-  some.textContent = "文字を変更"
-}
-first()
-function first() {
-  Flag();
-
-  if (Flag.trueEnd) {
-    End1();
-  } else {
-    End2();
-  }
-}
-
-
-function End1() {
-  const message = document.querySelector('.p');
-  message.textContent = "finish!"
-}
-
-function End2() {
-  const message = document.querySelector('.p');
-  message.textContent = "finish?"
-
-const canvas = document.getElementById('canvas');
-const c = canvas.getContext('2d');
-
-const width = canvas.width = window.innerWidth;
-const height = canvas.height = window.innerHeight;
-
-//フラグ管理
-function Flag() {
-  let flagjudge = [];
-  let trueEnd = false;
-
-  if (some) {
-    flagjudge[0] = true;
-  }
-
-  if (flagjudge[0]) {
-    trueEnd = true;
-  }
-}
-
-
-function first() {
-  Flag();
-
-  //ウィンドウをクリックでフラグを対処
-  document.body.onclick = Room1()
-
-  function Room1() {
-    const some = document.querySelector('.p');
-    some.textContent = "文字を変更"
-    Flag.flagjudge[0] = true;
-  }
-  
-  if (Flag.trueEnd) {
-    End1();
-  } else {
-    End2();
-  }
-}
-first()
-
-function End1() {
-  const message = document.querySelector('.p');
-  message.textContent = "finish!"
-}
-
-function End2() {
-  const message = document.querySelector('.p');
-  message.textContent = "finish?"
-}
+loop();
--- no3/scripts/main.js
+++ no3/scripts/main.js
@@ -3,17 +3,74 @@ window.onload = function() {
   const spinner = document.getElementById('loading');
   spinner.classList.add('loaded');
 
+var str = "...";
+document.getElementById('write').innerHTML = str;
+}
 
-//ふわっと
-var scrollAnimationElm = document.querySelectorAll('.sa');
-var scrollAnimationFunc = function() {
-  for(var i = 0; i < scrollAnimationElm.length; i++) {
-    var triggerMargin = 200;
-    if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
-      scrollAnimationElm[i].classList.add('show');
-    }
+
+
+
+//セリフ
+var text = [
+  "..................",
+  "急がなければ。",
+  "私の世界を待つ者がいる。",
+  "急がなければ。",
+  "はやる気持ちを無理やりに押さえつけ、努めて冷静にペンを走らせる。",
+  "-ドラマの脚本、私はその締め切りに追われている-",
+  "自ら望んだ道ではあるが、作品の肝となる脚本をたった一人で仕上げるという重圧と孤独は、時間と共に重く心に圧し掛かる",
+  "本来であれば同じ班員と共同で仕上げるべきものを、半ば押し付けられるようにして一任されたのだ。",
+  "辛くないと言ったら嘘になる。同じ大学生として、不真面目な彼らに腹が立たないと言えば、それも嘘になる。",
+  "それでも、こうして作業に没頭できているのは。やはり好きだからなのだろう。",
+  "図書館、病院、森、...。ドラマは場所を限定しない。突然大きな熊に襲われたり、気づけば世界を救っていたり、やろうと思えば思うように創造できる机上の世界。",
+  "カットも撮り直しもできない現実なんかより、ずっと自由だ。",
+  "『お前のドラマは現実味がない』とはよく言われるが、物語の中くらい現実離れしたっていいと私は思う。",
+  "だってその方が面白いから。",
+  " ",
+  "部屋の時計がカチリカチリと音を立てた。集中しているときにこそ、僅かな騒音がやけに大きく聞こえてしまうものだ。",
+  "通常よりもいくらか濃い筆圧が、無意識にもその音にストレスを感じていることを告げる。時刻は深夜の2時を回っていた。",
+  "？？「明日も早いし、もう寝るか。...というか寝たい。」",
+  "-欠伸",
+  "生理的に浮かぶ涙を手の甲で拭いつつ、机を整理する。書き殴った様な筆跡が目立つ脚本は、今日も未完成のまま閉じられた。",
+  "ふらつく足取りで、縋るように寝室へ続く扉のノブを捻る。",
+  "しかし、休息を望んだ一歩が地面に触れることは無かった。",
+  "暗転。",
+] 
+
+var n = 0;
+var max = text.length;
+
+var myfunc = function () {
+  document.getElementById('write').innerHTML = text[n];
+  n++;
+  if(n > max){
+    str = " ";
+    document.getElementById('write').innerHTML = str;
+    n = 0;
+    location.href = "index1.html";
   }
 }
-window.addEventListener('load', scrollAnimationFunc);
-window.addEventListener('scroll', scrollAnimationFunc);
+
+var text2 = [
+  "？？「...ここは？」",
+  "当然と言うべきか、返事はない。",
+  "辺りはほの暗く、周囲を見た渡してみても正確な情報が得られそうにない。",
+  "しかし、人間とは慣れの生物である。時間が経つに連れ環境に私の視界が適応し始めた。",
+  "そこは先ほどまでいた自室ではなく、ドアのみが立ち並ぶ無機質な空間であった。",
+  "振り返ってみても、そこには壁があるのみで入口らしき物は見当たらない。まるで寝室の扉の先が此処に繋がっていたかのような演出だ。",
+  "異常事態。極めて奇妙な体験をしている。それにも関わらず、私の心はこの状況に楽しみを感じ始めていた。",
+  "？？「面白い」",
+] 
+
+var max2 = text2.length;
+
+var myfunc2 = function () {
+  document.getElementById('write').innerHTML = text2[n];
+  n++;
+  if(n > max2){
+    str = " ";
+    document.getElementById('write').innerHTML = str;
+  }
 }
+
+
